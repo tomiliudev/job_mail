a5f5c38696928ebd453b3ff36c7b0742b4e9e44c
tomiliudev <tomiliudev@yahoo.co.jp>
2024-08-30 23:09:49 +0900

    レバテックのメールを取り込むように

--- Main.py
+++ Main.py
@@ -6,12 +6,17 @@ def main():
     my_mail = os.getenv('MY_MAIL')
     my_mail_password = os.getenv('MY_MAIL_PASSWORD')
     yahooMail = YahooMail(my_mail, my_mail_password)
-    emails = yahooMail.search_yahoo_mail('lassic.co.jp')
 
-    if emails:
-        my_webhook_url = os.getenv('MY_WEBHOOK_URL')
-        slack_notifier = SlackNotifier(my_webhook_url)
-        slack_notifier.send_notification(emails)
+    emails = []
+    domain_list = ['lassic.co.jp', 'levtech.jp']
+    for domain in domain_list:
+        emails = yahooMail.search_yahoo_mail(domain)
+        if emails:
+            emails.extend(emails)
+
+    my_webhook_url = os.getenv('MY_WEBHOOK_URL')
+    slack_notifier = SlackNotifier(my_webhook_url)
+    slack_notifier.send_notification(emails)
 
 if __name__ == "__main__":
     main()
